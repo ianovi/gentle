38d072f195756b04ba0792fb9f145ce12b9c316d
Igor A.Novitskiy <igor@speechpad.com>
2024-11-26 18:33:25 +0200

    Update setup.py to enhance packaging and dependencies
    Added `find_packages` for dynamic package inclusion and included `pyOpenSSL` and `wheel` in install and setup requirements respectively. Replaced `data_files` with `package_data` for resource inclusion and introduced `entry_points` to define console script entry. These changes enhance the overall robustness and usability of the package setup.

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from gentle import __version__
 
 setup(
-    app=['serve.py'],
-    data_files=[],
-    options={'py2app': {
-        'argv_emulation': False,
-        'resources': 'k3,m3,ffmpeg,www,exp'
-    }},
     name='gentle',
     version=__version__,
     description='Robust yet lenient forced-aligner built on Kaldi.',
     url='http://lowerquality.com/gentle',
     author='Robert M Ochshorn',
     license='MIT',
-    packages=['gentle'],
-    install_requires=['twisted'],
-    test_suite='tests',
+    packages=find_packages(),
+    install_requires=['twisted', 'pyOpenSSL'],
+    setup_requires=['wheel'],
+    entry_points={
+        'console_scripts': [
+            'gentle-serve=serve:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        '': ['k3', 'm3', 'ffmpeg', 'www', 'exp'],
+    },
 )
